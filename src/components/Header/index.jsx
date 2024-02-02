a55b26284223e121f844da5dc9d2ec9163c66fe2
Guillaume Bréchaire <g.brechaire@gmail.com>
2024-02-02 16:39:17 +0100

    styles: header fixed

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,34 @@
 import config from "../../data/index.json";
 
+// const Header = () => {
+//   const navigation = config.navigation;
+//   return (
+//     <div className="header">
+//       <div className="header__menu">
+//         <ul className="flex px-8 lg:px-32 gap-x-10 content-center leading-0 h-0">
+//           {navigation.map((item) => (
+//             <li className="mt-6 cursor-pointer" key={item.title}>
+//               <a href={`#${item.title}`}>
+//                 {item.title}
+//               </a>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Header;
+
 const Header = () => {
   const navigation = config.navigation;
   return (
-    <div className="header">
+    <div className="header fixed top-0 w-full z-50 bg-white shadow-md">
       <div className="header__menu">
-        <ul className="flex px-8 lg:px-32 gap-x-10 content-center leading-0 h-0">
+        <ul className="flex px-4 sm:px-8 lg:px-32 gap-x-10 content-center leading-0 h-10 items-center">
           {navigation.map((item) => (
-            <li className="mt-6 cursor-pointer" key={item.title}>
+            <li className="cursor-pointer" key={item.title}>
               <a href={`#${item.title}`}>
                 {item.title}
               </a>

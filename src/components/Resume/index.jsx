e08978b8395a52f498af026568660fa53e8207a1
Guillaume Bréchaire <g.brechaire@gmail.com>
2024-06-03 12:29:47 +0200

    Add cv pdf and dark

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -7,13 +7,13 @@ const Resume = ({ isDarkMode }) => {
     <div id='experiences' className={`w-full h-full flex flex-col items-center justify-center p-8 text-background font-mono ${!isDarkMode ? 'border-t':' border-t border-white'}`}>
       <Zoom>
         <img
-          src="/images/cv.jpg"
+          src={isDarkMode ? "/images/cv-dark.jpg" : "/images/cv.jpg"}
           alt="CV Guillaume Bréchaire"
-          className="shadow-lg max-w-full lg:max-w-4xl h-auto mb-4"
+          className="shadow-lg max-w-full lg:max-w-4xl h-auto mb-4 border-2 border-background rounded-lg cursor-pointer"
         />
       </Zoom>
       <div className="text-md text-center font-semibold p-0.5 gradient-background">
-        <a href="/images/cv.jpg" download="CV_Guillaume_Brechaire.jpg">
+        <a href="/pdf/CV_Guillaume_Brechaire.pdf" download="CV_Guillaume_Brechaire.pdf">
           <div className="text-background">
             <span className="block py-0.5 px-2 gradient-text">
               Télécharger le CV

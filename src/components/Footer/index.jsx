140e42d25b2e44a67d810f6609b0641451a00ac4
Guillaume Bréchaire <g.brechaire@gmail.com>
2024-06-16 20:06:44 +0200

    color footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -10,7 +10,7 @@ const Footer = ({ isDarkMode }) => {
 
   return (
     <footer className={`px-8 py-16 flex justify-center items-center flex-col ${!isDarkMode ? 'bg-neutral-900' : 'bg-[#111827]'}`}>
-      <div className="mx-auto text-3xl mb-8 space-x-10">
+      <div className="mx-auto text-3xl mb-8 space-x-10 text-neutral-300">
         <a rel="noreferrer" href={footer.linkedin} target="_blank" aria-label="Visitez mon profil LinkedIn">
           <i className="devicon-linkedin-plain"></i>
           <span className="sr-only">LinkedIn</span>

7f83180afd793fad5f13f721c1322a4267351883
Guillaume Bréchaire <g.brechaire@gmail.com>
2024-07-10 11:21:53 +0200

    Add Required for Contact input

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -162,6 +162,7 @@ const Contact = () => {
                                         id="first-name"
                                         value={firstName}
                                         onChange={(e) => setFirstName(e.target.value)}
+                                        required
                                         autoComplete="given-name"
                                         className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     />
@@ -179,6 +180,7 @@ const Contact = () => {
                                         id="last-name"
                                         value={lastName}
                                         onChange={(e) => setLastName(e.target.value)}
+                                        required
                                         autoComplete="family-name"
                                         className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     />
@@ -196,6 +198,7 @@ const Contact = () => {
                                         id="email"
                                         value={email}
                                         onChange={(e) => setEmail(e.target.value)}
+                                        required
                                         autoComplete="email"
                                         className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     />
@@ -230,6 +233,7 @@ const Contact = () => {
                                         value={message}
                                         onChange={(e) => setMessage(e.target.value)}
                                         rows={4}
+                                        required
                                         className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     />
                                     {errors?.message && <p className="mt-2 text-sm text-red-600">{errors.message}</p>}
